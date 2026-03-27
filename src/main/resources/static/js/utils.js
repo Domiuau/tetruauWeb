eed1b3d478971612461770837918075758e863cc
domiuau <domiuaunoob@outlook.com>
2026-03-26 22:56:39 -0300

    deixando as imagens mais leves

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -3,7 +3,7 @@ export function placeBackground() {
   const backgrounds = ["landscape1", "landscape2", "landscape3", "landscape4", "landscape5", "landscape6", "landscape7", "landscape8", "landscape9",]
 
   const randomBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
-  document.getElementById('background-overlay').style.backgroundImage = `url("../assets/${randomBg}.jpg")`;
+  document.getElementById('background-overlay').style.backgroundImage = `url("../assets/${randomBg}.webp")`;
 
 
 }
